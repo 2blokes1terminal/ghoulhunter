5f471b0c60abbc444fd8bd9f6465ce92841155e8
lockness-Ko <videolk9373@gmail.com>
2023-08-16 21:40:41 +1000

    (feat) add nrd download and keyword regex search

---
+++ src/ghoulhunter.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+from scanner import scanner
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(prog='ghoulhunter', description='hunt for phishing sites')
+
+    parser.add_argument('--brand-keywords', '-k', nargs='+', help='regex expression keywords', required=True)
+
+    args = parser.parse_args()
+
+    results = scanner.scan(args.brand_keywords)
+    print(results)
+
+if __name__ == "__main__":
+    main()
---
+++ src/scanner/scanner.py
@@ -0,0 +1,40 @@
+from io import BytesIO
+import requests
+import datetime
+import zipfile
+import base64
+import re
+
+def scan(keywords):
+    now = datetime.datetime.now()
+    month = str(now.month).zfill(2)
+    datezip = base64.b64encode(f'{now.year}-{month}-{now.day - 1}.zip'.encode()).decode()
+
+    req = requests.get(f'https://www.whoisds.com//whois-database/newly-registered-domains/{datezip}/nrd')
+
+    content = BytesIO(req.content)
+    zip = zipfile.ZipFile(content)
+
+    for name in zip.namelist():
+        if name == "domain-names.txt":
+            domain_list = zip.read(name)
+    
+    try:
+        domain_list = domain_list.split()
+    except:
+        domain_list = []
+
+    results = []
+    for keyword in keywords:
+        regex = re.compile(keyword)
+
+        results.extend(list(filter(lambda domain: check_domain(regex, domain.decode()), domain_list)))
+    print(list(set(results)))
+    
+    # return domain_list
+
+def check_domain(regex, domain):
+    return re.search(regex, domain)
+
+if __name__ == "__main__":
+    pass
